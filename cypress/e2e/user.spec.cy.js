7f02cbbcdbe14ee1813b586a200315caaf40e1db
PieLohmann <pietra.calheirs@gmail.com>
2025-09-24 23:03:02 -0300

    Added page objects

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,15 +1,19 @@
-import userData from '../fixtures/userData'
 
-describe('Login', () => {
+import userData from '../fixtures/userData'
+import LoginPage from '../pages/loginPage' 
+import DashboardPage from '../pages/dashboardPage'
+import menuPage from '../pages/menuPage'
+
+const loginPage = new LoginPage()
+const dashboardPage = new DashboardPage ()
+const MenuPage = new menuPage()
+
+describe('LoginOrange HRM Test', () => {
 
   const selectorsList = {
-    usernameField: "[name='username']",
-    passwordField: "[name='password']",
-    loginButton: "[type='submit']",
-    sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
-    dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role= 'alert']",
-    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+  
+    
+  
     firstNameField: "[name='firstName']",
     midleNameField: "[name='middleName']",
     lastNameField: "[name='lastName']",
@@ -17,31 +21,38 @@ describe('Login', () => {
     dateField: "[placeholder='yyyy-mm-dd']",
     dateCloseButton: ".--close",
     submitButton: "[type='submit']",
+    genericComboBox: ".oxd-select-wrapper",
+    
     
   }
 
 
   it.only('User Info Update - Success', () => {
-    cy.visit('/auth/login');
-    cy.get(selectorsList.usernameField).type(userData.userSuccess.username);
-    cy.get(selectorsList.passwordField).type(userData.userSuccess.password);
-    cy.get(selectorsList.loginButton).click();
-    cy.location('pathname').should('eq', '/web/index.php/dashboard/index'); 
-    cy.get(selectorsList.dashboardGrid);
-    cy.get(selectorsList.myInfoButton).click()
-    cy.get(selectorsList.firstNameField).clear().type('Roberto')
-    cy.get(selectorsList.midleNameField).clear().type('Santos')
-    cy.get(selectorsList.lastNameField).clear().type('Silva')
-    cy.get(selectorsList.genericFIeld).eq(4).clear().type('EmployTest')
-    cy.get(selectorsList.genericFIeld).eq(5).clear().type('OtherIdTest')
-    cy.get(selectorsList.genericFIeld).eq(6).clear().type('DriversLiceTest123')
-    cy.get(selectorsList.genericFIeld).eq(7).clear().type('2025-09-15')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.submitButton).eq(0).click()
-    cy.get('body').should('contain','Successfully Updated')
-    cy.get('.oxd-toast-close')
-
-  })
+    loginPage.accessLoginPage()
+    loginPage.loginWithUser(userData.userSuccess.username,userData.userSuccess.password)
+    dashboardPage.checkDashboardPage()
+    MenuPage.accessMyInfo()
+    
+  
+  //cy.get(selectorsList.firstNameField).clear().type('Roberto')
+  //cy.get(selectorsList.midleNameField).clear().type('Santos')
+  //cy.get(selectorsList.lastNameField).clear().type('Silva')
+  //cy.get(selectorsList.genericFIeld).eq(4).clear().type('EmployTest')
+  //cy.get(selectorsList.genericFIeld).eq(5).clear().type('OtherIdTest')
+  //cy.get(selectorsList.genericFIeld).eq(6).clear().type('DriversLiceTest123')
+  //cy.get(selectorsList.genericFIeld).eq(7).clear().type('2025-09-11')
+  //cy.get(selectorsList.dateCloseButton).click()
+  //cy.get(selectorsList.genericComboBox).eq(0).click() //nationality
+  //cy.get('.oxd-select-dropdown > :nth-child(6)').click()
+  //cy.get(selectorsList.genericComboBox).eq(1).click() //maritalStatus
+  //cy.get('.oxd-select-dropdown > :nth-child(2)').click()
+  //cy.get(selectorsList.submitButton).eq(0).click()
+  //cy.get('body').should('contain','Successfully Updated')
+  //cy.get('.oxd-toast-close')
+  //cy.get(selectorsList.genericComboBox).eq(2).click() //bloodType
+  //cy.get('.oxd-select-dropdown > :nth-child(6)').click()
+  //cy.get(selectorsList.genericFIeld).eq(15).type('TestTest')
+  
 
   it('User Info Update - Fail', () => { 
   cy.visit('/auth/login');
@@ -49,5 +60,6 @@ describe('Login', () => {
   cy.get(selectorsList.passwordField).type(userData.userFail.password);
   cy.get(selectorsList.loginButton).click();
   cy.get(selectorsList.wrongCredentialAlert).should('contain', 'Invalid credentials');
+  })
   })
 })
---
+++ cypress/pages/myInfoPage.js
@@ -0,0 +1,22 @@
+class MyInfoPage {
+
+    selectorsList(){
+        const selectors = {
+        firstNameField: "[name='firstName']",
+        midleNameField: "[name='middleName']",
+        lastNameField: "[name='lastName']",
+        genericFIeld: ".oxd-input-group",
+        dateField: "[placeholder='yyyy-mm-dd']",
+        dateCloseButton: ".--close",
+        submitButton: "[type='submit']",
+        genericComboBox: ".oxd-select-wrapper",
+        }
+
+        return selectors
+
+    }
+    
+    
+    
+
+}
